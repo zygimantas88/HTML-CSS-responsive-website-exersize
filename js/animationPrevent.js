932c8e9f80b14be11490ec5dd94982a0f398e470
zygimantas88 <57908890+zygimantas88@users.noreply.github.com>
2021-06-02 11:19:47 +0300

    Add files via upload

---
+++ js/animationPrevent.js
@@ -0,0 +1,4 @@
+// Užkrovus puslapį nuimama klasė ribojanti transition savybių veikimą
+window.addEventListener('load', function () {
+  document.querySelector('body').removeAttribute('class');
+});
